d2183fdeadb4bb6459d89fde14d5f7d19e66939f
Matthew Christopher Taggart <mtaggart89@gmail.com>
2016-05-21 16:29:23 -0400

    Created basic repo w/ packages.json and got basic node server running.

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var PORT = 3000;
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.listen(PORT, function () {
+  console.log('Server listening on PORT %s', PORT);
+});
