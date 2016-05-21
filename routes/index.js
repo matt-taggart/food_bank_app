812e44a5eb0df40affb6af737f8f326042b24ac9
long phan <lphan1989@gmail.com>
2016-05-21 18:27:50 -0400

    test routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
 
 router.get('/', function(req, res) {
   res.send('Hello World');
 });
+
+
+router.post('/login', function(req, res) {
+  res.sendFile(path.resolve('public/loginTest.html'));
+});
+
+router.post('/register', function(req, res) {
+  res.sendFile(path.resolve('public/registerTest.html'));
+});
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require('dotenv').config({silent: true});
 var express = require('express');
 var routes = require('./routes/index.js');
 var app = express();
-
 var PORT = 3000;
 
 app.use('/', routes);
+app.use('/login', routes);
+app.use('/register', routes);
+
 
 app.listen(PORT, function () {
   console.log('Server listening on PORT %s', PORT);
