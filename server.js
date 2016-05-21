6c23411f2f3d701b1441597f1bb45f11c136d032
eliezerwohl <eliezerwohl@gmail.com>
2016-05-21 18:19:31 -0400

    serverMongooseAdd

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ var app = express();
 
 var PORT = 3000;
 
+var logger = require("morgan");
+var bodyParser = require("body-parser")
+var mongoose = require('mongoose');
+
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(bodyParser.json());
+
+require("./server/models/db");
+
 app.use('/', routes);
 
 app.listen(PORT, function () {
